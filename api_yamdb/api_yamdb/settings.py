7ec7461cf675abf0f93f83db4c6a34664be82a70
Sergey Karagodin <patridon87@yandex.ru>
2021-10-04 20:55:53 +0300

    added user model, settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'reviews',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -101,3 +104,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+AUTH_USER_MODEL = 'reviews.User'
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    roles = ('user', 'moderator', 'admin')
+    bio = models.TextField(
+        verbose_name='Биография', 
+        blank=True
+        )
+    role = models.CharField(choices=roles)
+    
