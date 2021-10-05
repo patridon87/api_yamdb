4fdf3d54cf6f39a52995a4d5a3eea38838903db4
Sergey Karagodin <patridon87@yandex.ru>
2021-10-05 21:36:10 +0300

    models fixes

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -56,65 +56,6 @@ class ConfirmationCode(models.Model):
     code = models.CharField(max_length=4)
 
 
-class Review(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор отзыва',
-    )
-    text = models.TextField(null=False, verbose_name='Отзыв')
-    pub_date = models.DateTimeField(
-        verbose_name='Дата публикации', auto_now_add=True
-    )
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Произведение',
-    )
-    score = models.IntegerField(
-        validators=[
-            MinValueValidator(1, 'Оценка не может быть ниже 1'),
-            MaxValueValidator(10, 'Оценка не может быть больше 10')
-        ],
-        verbose_name='Оценка'
-    )
-
-    class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
-
-    def __str__(self):
-        return f'{self.text}'
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор комментария',
-    )
-    text = models.TextField(verbose_name='Комментарий')
-    pub_date = models.DateTimeField(
-        verbose_name='Дата публикации', auto_now_add=True
-    )
-    review = models.ForeignKey(
-        Review,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Отзыв',
-    )
-
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return self.text
-
-
 class Category(models.Model):
     """Категория произведения."""
     name = models.CharField(max_length=256, choices=CATEGORIES)
@@ -204,3 +145,63 @@ class GenreTitle(models.Model):
 
     def __str__(self):
         return f'{self.genre} {self.title}'
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва',
+    )
+    text = models.TextField(null=False, verbose_name='Отзыв')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации', auto_now_add=True
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+    )
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1, 'Оценка не может быть ниже 1'),
+            MaxValueValidator(10, 'Оценка не может быть больше 10')
+        ],
+        verbose_name='Оценка'
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return f'{self.text}'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария',
+    )
+    text = models.TextField(verbose_name='Комментарий')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации', auto_now_add=True
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
+        
