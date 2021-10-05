f6f0a87ada12399c35ca5796623f6e5a72d28cb0
Sergey Karagodin <patridon87@yandex.ru>
2021-10-05 18:55:15 +0300

    merge branches

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
@@ -10,13 +10,6 @@ class User(AbstractUser):
         blank=True
         )
     role = models.CharField(choices=roles)
-    
-=======
-from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models
-
-User = get_user_model()
 
 
 class Title(models.Model):
@@ -80,4 +73,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
->>>>>>> igor
