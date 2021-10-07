557033225de93bc88712e346fd1d670bc9f7ccab
Арапат <fresco2209@gmail.com>
2021-10-07 09:40:39 +0300

    Create permission IsAuthenticatedOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,3 +11,16 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_staff)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    message = "Changing someone else's content is not allowed!"
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
