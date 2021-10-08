db99279568d31a87e9eaddc24fa19bcc596edef9
ArapatSunny <78870545+ArapatSunny@users.noreply.github.com>
2021-10-08 18:45:27 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,7 +10,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_stuff)
+                or request.user.is_staff)
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
